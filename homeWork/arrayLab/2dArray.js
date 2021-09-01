97f1bf99202182cafa7ccbd45ea58470a2f0e3d0
ALI <ahmedaliou94@gmail.com>
2021-09-01 03:11:57 -0400

    react assignment

--- homeWork/arrayLab/2dArray.js
+++ homeWork/arrayLab/2dArray.js
@@ -3,7 +3,7 @@ function hourglassSum(arr) {
     let counter = -1000 
     for( let i=0 ; i< 4 ; i++){ 
         for( let j=0; j < 4 ; j++){
-            let hourGlass = (arr[i][j] + arr[i][j+1] + arr[i][j+2]  +arr[i+1][j+1] + arr[i+2][j]+ arr[i+2][j+1] + arr[i+2][j+2]
+            let hourGlass = (arr[i][j] + arr[i][j+1] + arr[i][j+2] +arr[i+1][j+1] + arr[i+2][j]+ arr[i+2][j+1] + arr[i+2][j+2]
     )
             if (hourGlass > counter) counter = hourGlass }}
     return counter}
--- homeWork/forloops.js
+++ homeWork/forloops.js
@@ -11,21 +11,21 @@ for(r=1; r<=5; r++){
 
 console.log("-------")
 
-// for(i=5; i>=1; i--){
+for(i=5; i>=1; i--){
 
-//     for(j=i; j>=1; j--)
-//     {
-//         process.stdout.write(j.toString())
-//     }
-//     console.log()
-// }
+    for(j=i; j>=1; j--)
+    {
+        process.stdout.write(j.toString())
+    }
+    console.log()
+}
 
 
 console.log("-------")
 
-for(i=0; i < 5; i++){
+for(i=j+1; i < 5; i++){
 
-    for(j=i+1; j< 5; j++)
+    for(j=1; j< i; j++)
     {
        
         process.stdout.write(j.toString())
