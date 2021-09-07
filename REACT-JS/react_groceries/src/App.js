a4ddefc8f3a6c73a08640c38f0638e3031dc088b
ALI <ahmedaliou94@gmail.com>
2021-09-07 12:51:15 -0400

    groceries list assignment

---
+++ REACT-JS/react_groceries/src/App.js
@@ -0,0 +1,14 @@
+import FormControlClass from "./Form";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3> Grocery App</h3>
+
+      <FormControlClass />
+    </div>
+  );
+}
+
+export default App;
