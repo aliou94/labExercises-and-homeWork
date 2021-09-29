00dc7c9b8b0a135244a2d7c88e127abdf17117b1
ALI <ahmedaliou94@gmail.com>
2021-09-29 12:51:10 -0400

    java lab

---
+++ Java/Main.java
@@ -0,0 +1,73 @@
+
+
+
+public class Main {
+   
+
+  public static void main(String args[]) {
+        
+// 1. Add, subtract, multiply, and divide (quotient & remainder) two numbers and display the output
+
+
+int x = 1, y = 2;
+float z = x;
+float w = y;
+float divSum = z/w;
+
+
+
+// System.out.println(x + y);
+// System.out.println(x - y);
+// System.out.println(x * y);
+// System.out.println(divSum);
+
+
+// 2. Swap two numbers: Store two numbers in separate variables, display their initial values, swap them, and display them again.
+
+int temp = x;
+x = y;
+y = temp;
+
+
+// System.out.println(x);
+// System.out.println(y);
+
+
+// 3. Display the average of five numbers: Create five separate variables holding different numbers and find the average
+
+
+
+
+// 4. Write a Java program that display the larger of two integers. Store these numbers in variables.
+
+// 5. Display the largest of three numbers. 
+
+int a = 2, b = 3, c = 4;
+
+
+
+
+
+
+
+// int largestNum = Math.max([a,b,c]);
+
+System.out.println(Math.max(Math.max(a,b), c));
+
+
+
+
+
+// 6. Determine if a number is negative, positive, or zero. 
+
+// 7. Determine if a number is even or odd. 
+
+// 8. Store a value in a variable called age. Determine if a person with the given age is eligible to vote (18 years old)
+
+
+
+
+    
+  }
+
+}
